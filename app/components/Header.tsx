5321f6b113a67ff7acf3557d968ad3a867455920
Anderson Sousa <and3rsonsousa@outlook.com>
2023-02-27 01:19:19 -0300

    fixes

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -20,32 +20,34 @@ export default function Header() {
 
 	return (
 		<motion.div
-			className="header p-2 fixed top-0 right-0 left-0 z-10"
+			className="fixed top-0 right-0 left-0 z-10"
 			initial={{ opacity: 0 }}
 			animate={{ opacity: 1, transition: { delay: 0.5 } }}
 		>
-			<div className="container mx-auto flex items-center justify-between">
-				<Link to="/">
-					<img
-						src="/logo.png"
-						alt="Mandacaru Empreendimentos"
-						className="logo-main"
-					/>
-					<img
-						src="/logo-white.png"
-						alt="Mandacaru Empreendimentos"
-						className="logo-white"
-					/>
-				</Link>
-				<div className="hidden menu lg:flex gap-2">
-					<Link to="/">Home</Link>
-					<Link to="/a-empresa">A empresa</Link>
-					<Link to="/o-que-fazemos">O que fazemos</Link>
-					<Link to="/obras">Obras</Link>
-					<Link to="/trabalhe-conosco">Trabalhe Conosco</Link>
-					<Link to="/fale-conosco" className="feature">
-						Fale Conosco
+			<div className="header p-2 relative">
+				<div className="container  mx-auto z-10 flex items-center justify-between">
+					<Link to="/">
+						<img
+							src="/logo.png"
+							alt="Mandacaru Empreendimentos"
+							className="logo-main"
+						/>
+						<img
+							src="/logo-white.png"
+							alt="Mandacaru Empreendimentos"
+							className="logo-white"
+						/>
 					</Link>
+					<div className="hidden menu lg:flex gap-2">
+						<Link to="/">Home</Link>
+						<Link to="/a-empresa">A empresa</Link>
+						<Link to="/o-que-fazemos">O que fazemos</Link>
+						<Link to="/obras">Obras</Link>
+						<Link to="/trabalhe-conosco">Trabalhe Conosco</Link>
+						<Link to="/fale-conosco" className="feature">
+							Fale Conosco
+						</Link>
+					</div>
 				</div>
 			</div>
 		</motion.div>
